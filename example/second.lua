4e914d8399b9e74db69abe285a2c818b5ae21342
Eric Mertens <emertens@gmail.com>
2022-02-06 11:25:19 -0800

    example

---
+++ example/second.lua
@@ -0,0 +1,8 @@
+{
+    smartlua = '0.1',
+    previous = '88f3a298d3fb136b6ab09712def6b3efd616a9c59af15224903a636b1f640718',
+    signers = {
+        '9LtcVo7q+at5ihEYtbu+YScb6qlbTt4D7GwlO+3kc+k=',
+    },
+    code = [[print('ok') transfer('68A3esZ9Ausdi05f6OCKs0e4qdwsbGlrSRzcHQ7WLEY=', 5)]],
+}
---
+++ example/third.lua
@@ -0,0 +1,7 @@
+{
+    smartlua = '0.1',
+    previous = '7493150d9da8b087d27efec366ba27fd3ebf10db49fc5fac03ec19901946d334',
+    signers = {},
+    code = [[read('68A3esZ9Ausdi05f6OCKs0e4qdwsbGlrSRzcHQ7WLEY=')]],
+}
+    
